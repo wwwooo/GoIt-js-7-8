27ef03ad3a200a36ee5bfd317fac928cbfd1814b
wwwooo <malinovsvska@gmail.com>
2016-09-10 23:41:14 +0300

    added tooltip

--- js/main.js
+++ js/main.js
@@ -6,13 +6,25 @@ $(function() {
         $('.tabs__content').hide();
         $('#content-one').show();
     });
+
     $('#header-two').on('click', function() {
         $('.tabs__content').hide();
         $('#content-two').show();
     });
+
     $('#header-three').on('click', function() {
         $('.tabs__content').hide();
         $('#content-three').show();
+    });
+
+    $('.hover').hide();
+
+    $('.user-data').mouseover(function() {
+        $(this).siblings('.hover').show().fadeOut(3000);
+    });
+
+    $('.user-data').mouseout(function() {
+        $(this).siblings('.hover').hide();
     });
 });
 
